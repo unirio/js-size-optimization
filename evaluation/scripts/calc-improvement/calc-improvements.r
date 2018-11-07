202cd679607d64c177954e8741510d612f7230c9
Marcio Barros <marcio.barros@uniriotec.br>
2018-11-07 16:33:15 -0200

    New scripts in the evaluation section

--- evaluation/scripts/calc-improvement/calc-improvements.r
+++ evaluation/scripts/calc-improvement/calc-improvements.r
@@ -1,7 +1,7 @@
 # Setup
 rm(list = ls());
 library("tidyverse");
-baseDir <- "/Users/Marcio/Desktop/Codigos/js-size-optimization/evaluation/scripts";
+baseDir <- "/Users/Marcio.barros/Desktop/Codigos/js-size-optimization/evaluation/scripts";
 
 # Load instance characteristics
 instancesFilename <- paste(baseDir, "/instance-data/instances.csv", sep="");
@@ -10,29 +10,29 @@ instances <- read_delim(instancesFilename, delim=",");
 # Load FAHC results
 fahcFilename <- paste(baseDir, "/../FAHC/results.csv", sep="");
 fahc <- read_delim(fahcFilename, delim=";");
+fahc <- fahc %>% mutate(imp = 100 * diffchars / originalchars);
 
 # Load SFAHC results
 sfahcFilename <- paste(baseDir, "/../SFAHC/results.csv", sep="");
 sfahc <- read_delim(sfahcFilename, delim=";");
+sfahc <- sfahc %>% mutate(imp = 100 * diffchars / originalchars);
 
 # Load SHC results
 shcFilename <- paste(baseDir, "/../SHC/results.csv", sep="");
 shc <- read_delim(shcFilename, delim=";");
+shc <- shc %>% mutate(imp = 100 * diffchars / originalchars);
 
 # Prepare consolidated data
 fahc_consolidate <- fahc %>%
 	group_by(Lib) %>% 
-	mutate(imp = 100 * diffchars / originalchars) %>%
 	select(Lib, imp);
 
 sfahc_consolidate <- sfahc %>% 
 	group_by(Lib) %>% 
-	mutate(imp = 100 * diffchars / originalchars) %>%
 	summarize(sfahc_mean = mean(imp), sfahc_sd = sd(imp), sfahc_median = median(imp), sfahc_max = max(imp));
 	
 shc_consolidate <- shc %>% 
 	group_by(Lib) %>% 
-	mutate(imp = 100 * diffchars / originalchars) %>%
 	summarize(shc_mean = mean(imp), shc_sd = sd(imp), shc_median = median(imp), shc_max = max(imp));
 
 # Generate results
@@ -40,3 +40,39 @@ improvements <- fahc_consolidate %>%
 	inner_join(sfahc_consolidate) %>%
 	inner_join(shc_consolidate) %>%
 	arrange(Lib);
+
+# Save main results
+improvementsFilename <- paste(baseDir, "/calc-improvement/improvements.csv", sep="");
+write.table(improvements, file = improvementsFilename, quote=FALSE, row.names=FALSE);
+
+# SHC x DFACH
+focusInstances <- c("uuid", "xml2js");
+
+for (instance_ in focusInstances) {
+	wshc <- shc %>% filter(Lib == instance_);
+	wfahc <- fahc %>% filter(Lib == instance_);
+	pValue <- wilcox.test(wshc$imp, mu = wfahc$imp)$p.value;
+	print(paste("SHC x DFAHC: pValue(", instance_, ") = ", pValue, sep=""));
+}
+
+# SFAHC x DFACH
+focusInstances <- c("d3-node", "decimal.js", "exectimer", "express", "jquery", "mathjs", "minimist", "node-semver", "tleaf", "UglifyJS2", "uuid");
+
+for (instance_ in focusInstances) {
+	wsfahc <- sfahc %>% filter(Lib == instance_);
+	wfahc <- fahc %>% filter(Lib == instance_);
+	pValue <- wilcox.test(wsfahc$imp, mu = wfahc$imp)$p.value;
+	print(paste("SFAHC x DFAHC: pValue(", instance_, ") = ", pValue, sep=""));
+}
+
+# Analise de correlacao
+improvementsToCorrelation <- improvements %>% inner_join(instances, by=c("Lib" = "lib"));
+
+cor(improvementsToCorrelation$sfahc_mean, improvementsToCorrelation$loc, method="spearman");
+cor(improvementsToCorrelation$sfahc_mean, improvementsToCorrelation$tests, method="spearman");
+cor(improvementsToCorrelation$sfahc_mean, improvementsToCorrelation$cov, method="spearman");
+
+cor(improvementsToCorrelation$imp - improvementsToCorrelation$sfahc_median, improvementsToCorrelation$loc, method="spearman");
+cor(improvementsToCorrelation$imp - improvementsToCorrelation$sfahc_median, improvementsToCorrelation$tests, method="spearman");
+cor(improvementsToCorrelation$imp - improvementsToCorrelation$sfahc_median, improvementsToCorrelation$cov, method="spearman");
+
