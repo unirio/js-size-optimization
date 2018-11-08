fd96fd48e3ab1f0fd4b9e6984ff11da23b8fc456
Marcio Barros <marcio.barros@uniriotec.br>
2018-11-08 20:23:13 -0200

    Advances in scripts for Table 3 and barcode

--- exploratory-study/scripts/calc-barcode/create-barcode-chart.r
+++ exploratory-study/scripts/calc-barcode/create-barcode-chart.r
@@ -2,39 +2,67 @@
 rm(list = ls());
 library("tidyverse");
 library("barcode");
-baseDir <- "/Users/Marcio/Desktop/Codigos/js-size-optimization/exploratory-study/scripts";
+baseDir <- "/Users/Marcio.barros/Desktop/Codigos/js-size-optimization/exploratory-study/scripts";
 
-#
-# Load files
-#
+# Load instance characteristic files
 instancesFilename <- paste(baseDir, "/instance-data/instances.csv", sep="");
 instances <- read_delim(instancesFilename, delim=",");
 
+# Load patches
 patchesFilename <- paste(baseDir, "/calc-list-patches/patches.csv", sep="");
 patches <- read_delim(patchesFilename, delim="\t");
 
-#
-# Prepare data
-#
+# Filter for the representative instances
 data <- patches %>% 
-		inner_join(instances, by_x="lib", by_y="lib") %>% 
-		#mutate(size = finish - start + 1) %>% 
-		#mutate(psize = 100 * size / loc) %>% 
-		mutate(pstart = 100 * start / loc) %>% 
-		mutate(pfinish = 100 * finish / loc);
+		inner_join(instances, by_x="lib", by_y="lib") %>%
+		select(lib, round, start, finish, loc);
+		
+lodash <- data %>% 
+		filter(lib == "lodash" & round == 42);
 
-#
-# Filter for the better results of LODASH and UUID
-#
-lodash <- data %>% filter(lib == "lodash" & round == 42);
-uuid <- data %>% filter(lib == "uuid" & round == 41);
+uuid <- data %>% 
+		filter(lib == "uuid" & round == 41);
+		
+# Add intermediate changed lines for UUID
+ruuid <- tibble(lib=character(), start=numeric());
 
-#
+for (index in 1:nrow(uuid)) {
+	start <- as.numeric(uuid[index, "start"]);
+	finish <- as.numeric(uuid[index, "finish"]);
+	loc <- as.numeric(uuid[index, "loc"]);
+	
+	for (i in start:finish) {
+		ruuid <- add_row(ruuid, lib="uuid", start=i / loc);
+	}
+}
+		
+# Add intermediate changed lines for LODASH
+rlodash <- tibble(lib=character(), start=numeric());
+
+for (index in 1:nrow(lodash)) {
+	start <- as.numeric(lodash[index, "start"]);
+	finish <- as.numeric(lodash[index, "finish"]);
+	loc <- as.numeric(lodash[index, "loc"]);
+	
+	for (i in start:finish) {
+		rlodash <- add_row(rlodash, lib="lodash", start=i / loc);
+	}
+}
+
+# Prepare percentile data
+ruuid <- ruuid %>% 
+		mutate(pstart = 100 * start / loc) %>%
+		select(lib, pstart);
+
+rlodash <- rlodash %>% 
+		mutate(pstart = 100 * start / loc) %>%
+		select(lib, pstart);
+
 # Plot the barcode chart
-#
-barcodeData <- list("lodash"=lodash$pstart, "uuid"=uuid$pstart);
+barcodeData <- list("lodash"=rlodash$pstart, "uuid"=ruuid$pstart);
 margins <- list(bottom = unit(2, "lines"), left = unit(1, "lines"), top = unit(1, "lines"), right = unit(1, "lines"));
 
-pdf("\\Users\\marcio.barros\\Desktop\\distribution.pdf", width=8, height=2);
+barcodeFilename <- paste(baseDir, "/calc-barcode/barcode.pdf", sep="");
+pdf(barcodeFilename, width=8, height=2);
 barcode(barcodeData, bcspace=0.8, outer.margins=margins);
 dev.off();
--- exploratory-study/scripts/calc-list-patches/calc-patch-size-distance.r
+++ exploratory-study/scripts/calc-list-patches/calc-patch-size-distance.r
@@ -3,18 +3,14 @@ rm(list = ls());
 library("tidyverse");
 baseDir <- "/Users/Marcio.Barros/Desktop/Codigos/js-size-optimization/exploratory-study/scripts";
 
-#
 # Load files
-#
 instancesFilename <- paste(baseDir, "/instance-data/instances.csv", sep="");
 instances <- read_delim(instancesFilename, delim=",");
 
 patchesFilename <- paste(baseDir, "/calc-list-patches/patches.csv", sep="");
 patches <- read_delim(patchesFilename, delim="\t");
 
-#
 # Prepare data
-#
 data <- patches %>% 
 		inner_join(instances, by_x="lib", by_y="lib") %>% 
 		mutate(size = finish - start + 1) %>% 
@@ -22,15 +18,28 @@ data <- patches %>%
 		mutate(pstart = 100 * start / loc) %>% 
 		mutate(pfinish = 100 * finish / loc);
 
-#
 # Size analysis
-#
 size <- data %>% 
 		group_by(lib) %>% 
 		summarize(meanSize = mean(psize, na.rm=TRUE), medianSize = median(psize));
+
+totalSize <- data %>% 
+		group_by(lib, round) %>% 
+		summarize(totalSize = sum(psize)) %>%
+		group_by(lib) %>% 
+		summarize(meanTotalSize = sum(totalSize) / 60);
 		
+size <- size %>%
+		inner_join(totalSize, by_x="lib", by_y="lib");
+
 overallSize <- data %>% 
 		summarize(meanSize = mean(psize, na.rm=TRUE), medianSize = median(psize));
+
+overallTotalSize <- data %>% 
+		group_by(lib, round) %>% 
+		summarize(totalSize = sum(psize));
+
+meanMeanTotalSize <- mean(overallTotalSize$totalSize);
 		
 sizeToCorrelation <- size %>% 
 		inner_join(instances, by_x="lib", by_y="lib") %>%
@@ -38,11 +47,10 @@ sizeToCorrelation <- size %>%
 
 correlationMeanSizeLoc <- cor(sizeToCorrelation$meanSize, sizeToCorrelation$loc, method="spearman");
 correlationMedianSizeLoc <- cor(sizeToCorrelation$medianSize, sizeToCorrelation$loc, method="spearman");
+correlationMeanTotalSizeLoc <- cor(sizeToCorrelation$meanTotalSize, sizeToCorrelation$loc, method="spearman");
 
 		
-#
 # Distance analysis
-#
 distance <- data %>% 
 		arrange(lib, round) %>% 
 		mutate(lastRound = lag(round, default=0)) %>%
