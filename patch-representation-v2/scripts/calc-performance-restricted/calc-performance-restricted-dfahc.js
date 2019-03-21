d62b35476dd574a0d829fd5a8ff5ca8b8d6a044f
Marcio Barros <marcio.barros@gmail.com>
2019-03-21 12:41:50 -0300

    Novos dados da PR

---
+++ patch-representation-v2/scripts/calc-performance-restricted/calc-performance-restricted-dfahc.js
@@ -0,0 +1,41 @@
+var fs = require('fs');
+
+var libs = 
+[
+	'exectimer', 
+	'minimist', 
+	'tleaf', 
+	'uuid'
+];
+
+var result = "";
+
+/*
+ * Processes one file
+ */
+function handleFile(item, index) {
+	var baseDir = "..\\evaluation\\dfahc\\" + item;
+	
+	var improvements = fs.readFileSync(baseDir + "\\HC\\0_modifications.csv", "utf8");
+	var lines = improvements.split("\n");
+	var len = lines.length;
+	
+	var line1 = lines[1];
+	var tokens = line1.split(";");
+	var originalSize = (tokens.length == 4 && tokens[2] == "original") ? parseInt(tokens[3]) : 0;
+	
+	var lineN = lines[len-2];
+	var tokens = lineN.split(";");
+	var reducedSize = (tokens.length == 4) ? parseInt(tokens[3]) : originalSize;
+	
+	var improvement = 100.0 - 100.0 * reducedSize / originalSize;
+	result += item + "\t" + 0 + "\t"+ originalSize + "\t" + reducedSize + "\t" + improvement + "\n";
+}
+
+/*
+ * Main program
+ */
+libs.forEach(handleFile);
+
+result = "lib\tround\toriginal\treduced\timprovement\n" + result;
+fs.writeFileSync("results\\performance-restricted-dfahc.csv", result, 'utf8');
