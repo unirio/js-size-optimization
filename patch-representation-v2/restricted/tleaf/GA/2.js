4df4e580549d5363e50d48a667f6acd14fbf63c2
Marcio Barros <marcio.barros@gmail.com>
2019-03-25 08:57:18 -0300

    Resultados restritos

---
+++ patch-representation-v2/restricted/tleaf/GA/2.js
@@ -0,0 +1,93 @@
+'use strict';
+var inquirer = require('inquirer');
+var _ = require('lodash');
+var config = require('./config');
+////////
+var ask = module.exports = {};
+////////
+ask.createUnit = function (callback) {
+    var questions = [
+        {
+            type: 'list',
+            name: 'type',
+            message: 'Unit type:',
+            choices: config.units.process
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Unit name:',
+            validate: required
+        },
+        {
+            type: 'input',
+            name: 'module',
+            message: 'Module name:',
+            validate: required
+        }
+    ];
+    inquirer.prompt(questions, function (answers) {
+        var unit = {
+            name: answers.name,
+            type: answers.type,
+            module: { name: answers.module },
+            deps: []
+        };
+        // unit.deps will be populated by reference
+        addUnitDependency(unit.deps, function () {
+            callback(unit);
+        });
+    });
+};
+ask.pickUnit = function (units, callback) {
+    if (units.length === 1) {
+        return callback(_.first(units));
+    }
+    var question = {
+        type: 'list',
+        name: 'unit',
+        message: 'What unit do you want to test?',
+        choices: function () {
+            return units.map(function (unit, index) {
+                return {
+                    name: [
+                        _.capitalize(unit.type),
+                        '"' + unit.name + '"',
+                        'from module',
+                        '"' + unit.module.name + '"'
+                    ].join(' '),
+                    value: index
+                };
+            });
+        }
+    };
+    inquirer.prompt(question, function (answer) {
+        var unit = units[answer.unit];
+        callback(unit);
+    });
+};
+ask.identifyDeps = function (deps, callback) {
+    if (!config.dependencies.process.length) {
+        return callback([]);
+    }
+    var questions = deps.map(function (dep, index) {
+        return;
+    });
+    inquirer.prompt(questions, function (answers) {
+        var identified = _.map(answers, function (type, index) {
+            var dep = deps[index];
+            dep.type = type;
+            return dep;
+        });
+        callback(identified);
+    });
+};
+function required(input) {
+    if (isEmptyString(input)) {
+        return 'Value is required';
+    }
+    return true;
+}
+function isEmptyString(string) {
+    return _.isEmpty(_.trim(string));
+}
